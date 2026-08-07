1a16f7f5b511d7eff0e9db95e95ffc3d71c25dc7
YasmimVitoria28 <yasmim.vsr07@aluno.ifsc.edu.br>
2026-08-07 15:17:43 -0300

    07/08/2026
    comit de garantia
    modificação em:
    -main
    -alunocontroller
    -alunostable
    -sidebar (criação)
    -web.php

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Aluno;
 
 class AlunoController extends Controller
 {
     public function index()
     {
-        return view('aluno.list');
+        $dados = Aluno::all();
+
+        return view('aluno.list',['dados',$dados]);
     }
 }
